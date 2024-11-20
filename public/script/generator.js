8390d287dc8c555bbcd9e6003dfb5fa20f2c76a1
Alexander <maybecode@proton.me>
2024-11-21 00:29:03 +0100

    adjusted room generation

--- public/script/generator.js
+++ public/script/generator.js
@@ -7,24 +7,19 @@ export function generateRoomInDirection(position, direction) {
     let roomSize = currentRoom.roomSize;
     let newX = currentRoom.x;
     let newY = currentRoom.y;
-    let newDoorDirections = [];
 
     switch (direction) {
-        case 's': // south
-            newY += roomSize + 1;
-            newDoorDirections = ['n'];
+        case 's':
+            newY += roomSize + 2;
             break;
-        case 'e': // east
-            newX += roomSize + 1;
-            newDoorDirections = ['w'];
+        case 'e':
+            newX += roomSize + 2;
             break;
-        case 'n': // north
-            newY -= roomSize + 1;
-            newDoorDirections = ['s'];
+        case 'n':
+            newY -= roomSize + 2;
             break;
-        case 'w': // west
-            newX -= roomSize + 1;
-            newDoorDirections = ['e'];
+        case 'w':
+            newX -= roomSize + 2;
             break;
         default:
             console.log("Ungültige Richtung: ", direction);
@@ -33,35 +28,18 @@ export function generateRoomInDirection(position, direction) {
 
     const roomExists = rooms.some(room => room.x === newX && room.y === newY);
     if (roomExists) {
-        location.playerX = newX + Math.floor(roomSize / 2);
-        location.playerY = newY + Math.floor(roomSize / 2);
-        console.log("Ein Raum existiert bereits an dieser Position.");
+        positionPlayerInCenterOfRoom(newX, newY, roomSize);
         return null;
     }
 
-    // Berechnung der Türen unter Berücksichtigung der Rückverlinkung
-    const oppositeDirection = getOppositeDirection(direction);
-    const doors = [...newDoorDirections, oppositeDirection];
-
-    // Neuen Raum erstellen
     const newRoom = new Room(newX, newY, roomSize, position);
 
-    console.log("Neuer Raum generiert: ", newRoom);
+    positionPlayerInCenterOfRoom(newX, newY, roomSize);
+
     return newRoom;
 }
 
-// Hilfsfunktion zur Bestimmung der entgegengesetzten Richtung
-function getOppositeDirection(direction) {
-    switch (direction) {
-        case 'n':
-            return 's';
-        case 's':
-            return 'n';
-        case 'e':
-            return 'w';
-        case 'w':
-            return 'e';
-        default:
-            return null;
-    }
+function positionPlayerInCenterOfRoom(roomX, roomY, roomSize) {
+    location.playerX = roomX + Math.floor(roomSize / 2);
+    location.playerY = roomY + Math.floor(roomSize / 2);
 }
